94be98997b1e5128a2d6cf98d8320ae207a8823d
camilomontoyau <camilomontoya2@gmail.com>
2020-04-13 17:11:26 -0500

    example5

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Example1 from './Example1';
 import Example2 from './Example2';
 import Example3 from './Example3';
 import Example4 from './Example4';
+import Example5 from './Example5';
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
         <Example2 />
         <Example3 />
         <Example4 />
+        <Example5 />
+        <div style={{minHeight:'500px'}}></div>
       </Container>
     </div>
   );
---
+++ src/Example5.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Dotdotdot from "react-dotdotdot";
+
+const useStyles = makeStyles({
+  root: {
+    width: "230px",
+  },
+});
+
+const Example5 = () => {
+  return (
+    <div>
+      <h1>example 5</h1>
+      <p></p>
+      <div style={{ maxWidth: "400px" }}>
+        <Dotdotdot clamp={2}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
+          iaculis metus. Etiam egestas sagittis tincidunt. Nunc dictum sapien
+          vel mauris ultrices bibendum. Integer volutpat eros ut purus auctor,
+          et eleifend tellus consectetur. Maecenas iaculis nisi sed turpis
+          ornare hendrerit. In vitae mauris quis ligula sodales luctus. Nullam
+          sodales sodales nunc, eu facilisis nunc sodales nec. Etiam luctus
+          sollicitudin mattis. Sed tempus erat vel dapibus molestie. Nulla
+          aliquet vel ligula ut consequat. Suspendisse euismod lectus eu enim
+          rhoncus dapibus. Vivamus consectetur feugiat nunc at tempus. Quisque
+          imperdiet in dui malesuada egestas. Cras elementum odio id risus
+          viverra viverra. Quisque interdum.
+        </Dotdotdot>
+      </div>
+    </div>
+  );
+};
+
+export default Example5;
