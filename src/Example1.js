58cb99ef9cc74d6c25f112cd3882c52fa67cc0ba
camilomontoyau <camilomontoya2@gmail.com>
2020-04-13 14:44:38 -0500

    Example 1

--- src/Example1.js
+++ src/Example1.js
@@ -1,41 +1,53 @@
 import React from "react";
-import Paper from '@material-ui/core/Paper';
-import MenuList from '@material-ui/core/MenuList';
-import MenuItem from '@material-ui/core/MenuItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Typography from '@material-ui/core/Typography';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
-import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
+import Paper from "@material-ui/core/Paper";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Typography from "@material-ui/core/Typography";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
+import PriorityHighIcon from "@material-ui/icons/PriorityHigh";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Example1 = () => (
-  <Paper className={classes.root}>
-    <h1>example 1</h1>
-    <MenuList>
-      <MenuItem>
-        <ListItemIcon>
-          <SendIcon fontSize="small" />
-        </ListItemIcon>
-        <Typography variant="inherit">A short message</Typography>
-      </MenuItem>
-      <MenuItem>
-        <ListItemIcon>
-          <PriorityHighIcon fontSize="small" />
-        </ListItemIcon>
-        <Typography variant="inherit">
-          A very long text that overflows
-        </Typography>
-      </MenuItem>
-      <MenuItem>
-        <ListItemIcon>
-          <DraftsIcon fontSize="small" />
-        </ListItemIcon>
-        <Typography variant="inherit" noWrap>
-          A very long text that overflows
-        </Typography>
-      </MenuItem>
-    </MenuList>
-  </Paper>
-);
+const useStyles = makeStyles({
+  root: {
+    width: 230,
+  },
+});
+
+const Example1 = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <h1>example 1</h1>
+      <Paper className={classes.root}>
+        <MenuList>
+          <MenuItem>
+            <ListItemIcon>
+              <SendIcon fontSize="small" />
+            </ListItemIcon>
+            <Typography variant="inherit">A short message</Typography>
+          </MenuItem>
+          <MenuItem>
+            <ListItemIcon>
+              <PriorityHighIcon fontSize="small" />
+            </ListItemIcon>
+            <Typography variant="inherit">
+              A very long text that overflows
+            </Typography>
+          </MenuItem>
+          <MenuItem>
+            <ListItemIcon>
+              <DraftsIcon fontSize="small" />
+            </ListItemIcon>
+            <Typography variant="inherit" noWrap>
+              A very long text that overflows
+            </Typography>
+          </MenuItem>
+        </MenuList>
+      </Paper>
+    </div>
+  );
+};
 
 export default Example1;
