86082d01e413ab872a8f8e7b375201e52546930c
camilomontoyau <camilomontoya2@gmail.com>
2020-04-13 16:15:06 -0500

    seems to not work inside <ul> <li>

--- src/Example3.js
+++ src/Example3.js
@@ -4,11 +4,14 @@ import MenuList from "@material-ui/core/MenuList";
 import MenuItem from "@material-ui/core/MenuItem";
 import { makeStyles } from "@material-ui/core/styles";
 import LinesEllipsis from "react-lines-ellipsis";
-import LinesEllipsisTooltip from "react-lines-ellipsis-with-tooltip";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
-    width: "230px",
+    width: "500px",
+  },
+  tRoot: {
+    width: "200px",
   },
 });
 
@@ -21,13 +24,15 @@ const Example3 = () => {
       <Paper className={classes.root}>
         <MenuList>
           <MenuItem>
-            <div style={{maxWidth:'200px'}}>
-                <LinesEllipsis
+            <Typography classes={{root: classes.tRoot}}>
+              <LinesEllipsis
                 text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac iaculis metus. Etiam egestas sagittis tincidunt. Nunc dictum sapien vel mauris ultrices bibendum. Integer volutpat eros ut purus auctor, et eleifend tellus consectetur. Maecenas iaculis nisi sed turpis ornare hendrerit. In vitae mauris quis ligula sodales luctus. Nullam sodales sodales nunc, eu facilisis nunc sodales nec. Etiam luctus sollicitudin mattis. Sed tempus erat vel dapibus molestie. Nulla aliquet vel ligula ut consequat. Suspendisse euismod lectus eu enim rhoncus dapibus. Vivamus consectetur feugiat nunc at tempus. Quisque imperdiet in dui malesuada egestas. Cras elementum odio id risus viverra viverra. Quisque interdum."
-                maxLine={2}
+                maxLine={3}
                 basedOn="letters"
-                />
-            </div>
+                component="span"
+                trimRight
+              />
+            </Typography>
           </MenuItem>
           <MenuItem>2</MenuItem>
           <MenuItem>3</MenuItem>
