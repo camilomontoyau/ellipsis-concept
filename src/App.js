66284b581ede78d7e66f4f54c4730b6edfeea1cc
camilomontoyau <camilomontoya2@gmail.com>
2020-04-13 16:04:15 -0500

    example4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Container from '@material-ui/core/Container';
 import Example1 from './Example1';
 import Example2 from './Example2';
 import Example3 from './Example3';
+import Example4 from './Example4';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Example1 />
         <Example2 />
         <Example3 />
+        <Example4 />
       </Container>
     </div>
   );
---
+++ src/Example4.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import LinesEllipsis from "react-lines-ellipsis";
+import LinesEllipsisTooltip from "react-lines-ellipsis-with-tooltip";
+
+const useStyles = makeStyles({
+  root: {
+    width: "230px",
+  },
+});
+
+const Example4 = () => {
+  return (
+    <div>
+      <h1>example 4</h1>
+      <p></p>
+      <div style={{ maxWidth: '200px', height: '300px' }}>
+        <LinesEllipsisTooltip
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac iaculis metus. Etiam egestas sagittis tincidunt. Nunc dictum sapien vel mauris ultrices bibendum. Integer volutpat eros ut purus auctor, et eleifend tellus consectetur. Maecenas iaculis nisi sed turpis ornare hendrerit. In vitae mauris quis ligula sodales luctus. Nullam sodales sodales nunc, eu facilisis nunc sodales nec. Etiam luctus sollicitudin mattis. Sed tempus erat vel dapibus molestie. Nulla aliquet vel ligula ut consequat. Suspendisse euismod lectus eu enim rhoncus dapibus. Vivamus consectetur feugiat nunc at tempus. Quisque imperdiet in dui malesuada egestas. Cras elementum odio id risus viverra viverra. Quisque interdum."
+          maxLine={3}
+          basedOn="letters"
+          trimRight
+          component="span"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Example4;
