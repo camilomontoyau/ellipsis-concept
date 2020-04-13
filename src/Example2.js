d755d292b1d79d5b3d1b35688f222e1dc1b355db
camilomontoyau <camilomontoya2@gmail.com>
2020-04-13 15:22:00 -0500

    example2

--- src/Example2.js
+++ src/Example2.js
@@ -2,16 +2,12 @@ import React from "react";
 import Paper from "@material-ui/core/Paper";
 import MenuList from "@material-ui/core/MenuList";
 import MenuItem from "@material-ui/core/MenuItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import Typography from "@material-ui/core/Typography";
-import DraftsIcon from "@material-ui/icons/Drafts";
-import SendIcon from "@material-ui/icons/Send";
-import PriorityHighIcon from "@material-ui/icons/PriorityHigh";
+import Dotdotdot from "react-dotdotdot";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   root: {
-    width: 230,
+    width: "230px",
   },
 });
 
@@ -20,25 +16,38 @@ const Example2 = () => {
   return (
     <div>
       <h1>example 2</h1>
+      <p>
+        the example below is using{" "}
+        <a href="https://www.npmjs.com/package/react-dotdotdot">
+          react-dotdotdot
+        </a>
+      </p>
       <Paper className={classes.root}>
         <MenuList>
           <MenuItem>
-            <ListItemIcon>
-              <SendIcon fontSize="small" />
-            </ListItemIcon>
-            <Typography variant="inherit">A short message</Typography>
+            <Dotdotdot clamp={"230px"}>
+              This text is using react-dotdotdot clamp = 2 This text is using
+              react-dotdotdot clamp = 2 This text is using react-dotdotdot clamp
+              = 2
+            </Dotdotdot>
           </MenuItem>
           <MenuItem>
-            <ListItemIcon>
-              <PriorityHighIcon fontSize="small" />
-            </ListItemIcon>
-            A very long text that overflows. A very long text that overflows
+            <Dotdotdot clamp={3}>
+              This text is using react-dotdotdot clamp = 3 This text is using
+              react-dotdotdot clamp = 3 This text is using react-dotdotdot clamp
+              = 3 This text is using react-dotdotdot clamp = 3 This text is
+              using react-dotdotdot clamp = 3 This text is using react-dotdotdot
+              clamp = 3
+            </Dotdotdot>
           </MenuItem>
           <MenuItem>
-            <ListItemIcon>
-              <DraftsIcon fontSize="small" />
-            </ListItemIcon>
-            A very long text that overflows. A very long text that overflows
+            <Dotdotdot clamp={4}>
+              This text is using react-dotdotdot clamp = 4 This text is using
+              react-dotdotdot clamp = 4 This text is using react-dotdotdot clamp
+              = 4 This text is using react-dotdotdot clamp = 4 This text is
+              using react-dotdotdot clamp = 4 This text is using react-dotdotdot
+              clamp = 4
+            </Dotdotdot>
           </MenuItem>
         </MenuList>
       </Paper>
