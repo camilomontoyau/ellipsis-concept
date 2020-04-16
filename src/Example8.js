5b6a566e182d757a2587b74f27862cc11206ae22
camilomontoyau <camilomontoya2@gmail.com>
2020-04-16 16:32:16 -0500

    example8 first try with react-truncate

--- src/Example8.js
+++ src/Example8.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ColumnResizer from "react-column-resizer";
 import Dotdotdot from "react-dotdotdot";
 import Button from "@material-ui/core/Button";
 import Tooltip from "@material-ui/core/Tooltip";
+import Truncate from "react-truncate";
 import "./example8.scss";
 
 const Example8 = () => {
@@ -17,6 +18,41 @@ const Example8 = () => {
   rhoncus dapibus. Vivamus consectetur feugiat nunc at tempus. Quisque
   imperdiet in dui malesuada egestas. Cras elementum odio id risus
   viverra viverra. Quisque interdum.`;
+
+  const parentRef = useRef(null);
+  const tableRef = useRef(null);
+  const resizerRef = useRef(null);
+  const tdRef1 = useRef(null);
+  const tdRef2 = useRef(null);
+  const [truncatedTextWidth, setTruncatedTextWidth] = useState(0);
+
+  console.log({parentRef, tableRef, resizerRef, tdRef1, tdRef2});
+  if(parentRef && parentRef.current) {
+    parentRef.current.onResize = (parentEvent) => {
+      console.log({parentEvent})
+    }
+  }
+  if(tableRef && tableRef.current) {
+    tableRef.current.onResize = (tableEvent) => {
+      console.log({tableEvent})
+    }
+  }
+  if(resizerRef && resizerRef.current) {
+    resizerRef.current.onResize = (resizerEvent) => {
+      console.log({resizerEvent})
+    }
+  }
+  if(tdRef1 && tdRef1.current) {
+    tdRef1.current.onResize = (tdEvent1) => {
+      console.log({tdEvent1})
+    }
+  }
+  if(tdRef2 && tdRef2.current) {
+    tdRef2.current.onResize = (tdEvent2) => {
+      console.log({tdEvent2})
+    }
+  }
+
   return (
     <div>
       <h1>example 8</h1>
@@ -25,10 +61,10 @@ const Example8 = () => {
         to parent node resizing <br />
       </p>
       <div style={{ maxWidth: "700px" }}>
-        <table>
+        <table ref={tableRef}>
           <tbody>
             <tr>
-              <td>
+              <td ref={tdRef1}>
                 <span>react dotdotdot example</span>
                 <Tooltip arrow title={text}>
                   <Button variant="outlined">
@@ -36,17 +72,17 @@ const Example8 = () => {
                   </Button>
                 </Tooltip>
               </td>
-              <ColumnResizer className="columnResizer" minWidth={0} />
-              <td>{text}</td>
+              <ColumnResizer ref={resizerRef} className="columnResizer" minWidth={0} />
+              <td ref={tdRef2}>{text}</td>
             </tr>
             <tr>
               <td>{text}</td>
               <td />
-              <td>
+              <td ref={parentRef}>
                 <span>react dotdotdot example</span>
                 <Tooltip arrow title={text}>
                   <Button variant="outlined">
-                    <Dotdotdot clamp={2}>{text}</Dotdotdot>
+                    <Truncate lines={1} width={truncatedTextWidth}>{text}</Truncate>
                   </Button>
                 </Tooltip>
               </td>
