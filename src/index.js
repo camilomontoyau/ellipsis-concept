783ba2728223449cf8f533d9468f49ab44ba0e36
camilomontoyau <camilomontoya2@gmail.com>
2020-05-14 16:15:00 -0500

    removiendo el polyfill del index principal

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import ResizeObserver from 'resize-observer-polyfill';
 
 ReactDOM.render(
   <BrowserRouter>
